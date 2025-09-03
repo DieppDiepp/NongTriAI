332194d3873e9c426bbd8f5ae50ba1bd71590ddd
DieppDiepp <Concatudio1@gmail.com>
2025-09-03 22:03:33 +0700

    Add missing file

---
+++ mapping.py
@@ -0,0 +1,7 @@
+# Mục đích chuyển plan_type về lại tiếng Việt để thêm vào query retrievel tăng hiệu xuất và tính xác định 
+
+PLANT_TYPE_MAPPING = {
+    "caphe": "cà phê",
+    "hotieu": "hồ tiêu",
+    "saurieng": "sầu riêng"
+}
